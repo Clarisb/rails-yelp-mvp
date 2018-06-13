04f939403841d27549a32713971ee1b8878020d0
clara schvartzman <c.s.benitez@outlook.com>
2018-06-13 14:59:04 +0200

    Routes & controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    resources :reviews, only: [ :new, :create ]
+  end
+  get "restaurants", to: "restaurants#index"
+  get "restaurants/:restaurant_id", to: "restaurants#show"
+  get "restaurants", to: "restaurants#new"
+  post "restaurants", to: "restaurants#create"
 end
+  # get "restaurants/:restaurant_id/reviews",     to: "reviews#index"
+  # get "restaurants/:restaurant_id/reviews/new", to: "reviews#new"
+  # post "restaurants/:restaurant_id/reviews",    to: "reviews#create"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Restaurant.create(name:"Pizza yolo", address:"blabla",phone_number:"02390239", category:"italian")
+Restaurant.create(name:"Pizza yolo", address:"Pizza Street",phone_number:"02390239", category:"italian")
+Restaurant.create(name:"Burgers.inc", address:"Bruger Street",phone_number:"02693239", category:"belgian")
+Restaurant.create(name:"Won Ki", address:"Chinatown",phone_number:"0234282", category:"chinese")
+Restaurant.create(name:"Baguette", address:"Baguette street",phone_number:"02356239", category:"french")
+Restaurant.create(name:"Wasabi", address:"Wasabi Street",phone_number:"027377239", category:"japanese")
